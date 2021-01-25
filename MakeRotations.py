cfcecfa317b5ffbb473878a6d94cd1322e77a76a
Mike1011 <michael.omalley1011@gmail.com>
2021-01-25 14:16:27 +0000

    Updated Package name

--- MakeRotations.py
+++ MakeRotations.py
@@ -2,23 +2,30 @@ import os
 import pickle
 from multiprocessing import Pool
 
-import DriftMLP
-from DriftMLP.rotations import random_ll_rot
+import driftmlp
+from driftmlp.rotations import random_ll_rot
+from driftmlp.drifter_indexing.discrete_system import h3_default
+import numpy as np
 
-rotations = [random_ll_rot() for i in range(10)]  #
+p = Pool(18)
+seed=10
+np.random.seed(seed)
+rotations = [random_ll_rot() for i in range(100)]  #
 file = os.environ['DRIFTFILE']
-
+res=4
+discretizer = h3_default(res=res)
 
 def get_network(rot):
     drift_kwargs = {'variables': ['position', 'drogue', 'datetime'],
                     'drop_na': False,
                     'drogue': True,
                     'lon_lat_transform': rot}
-    net = DriftMLP.network_from_file(fname=file, drift_kwargs=drift_kwargs)
+    net = DriftMLP.network_from_file(fname=file, drift_kwargs=drift_kwargs, discretizer = discretizer, visual=False)
+    net['discretizer'] = None
+    print(net['gpd'])
     return net
 
 
-p = Pool(4)
 to_store = list(p.map(get_network, rotations))
-pickle.dump(to_store, open(f'rotations_{len(rotations)}.p', 'wb'))
+pickle.dump(to_store, open(f'rotations_{len(rotations)}_{res}_{seed}.p', 'wb'))
 p.close()
